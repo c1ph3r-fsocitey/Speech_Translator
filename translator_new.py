b9ffc3caf9e9dc6d7124b9301cf08245cdce6f6a
C1PH3R.FSOCITEY <109020327+c1ph3r-fsocitey@users.noreply.github.com>
2023-06-25 12:06:05 +0530

    translator_new.py

--- translator_new.py
+++ translator_new.py
@@ -1,30 +1,30 @@
 import speech_recognition as sr
-from googletrans import Translator
+from translate import Translator
 from gtts import gTTS
 from playsound import playsound
 import os
 
-while True:  
+while True:
     r = sr.Recognizer()
-    translator = Translator()
+    translator = Translator(to_lang="fr")
     with sr.Microphone() as source:
-        print("speak now!")
+        print("Speak now!")
         audio = r.listen(source)
         try:
             speech_text = r.recognize_google(audio)
             print(speech_text)
-            if(speech_text == "exit"):
+            if speech_text == "exit":
                 break
-        except sr.UnknownValueError():
-            print("could not understand")
-        except sr.RequestError():
-            print("cold not understand")
-        translated_text = translator.translate(speech_text, dest="fr")
-        translated_text_only = translated_text.text
-        
-        print(translated_text_only)
-        
-        voice = gTTS(translated_text_only, lang="fr")
+        except sr.UnknownValueError:
+            print("Could not understand.")
+        except sr.RequestError:
+            print("Could not request results.")
+
+        translated_text = translator.translate(speech_text)
+
+        print(translated_text)
+
+        voice = gTTS(text=translated_text, lang="fr")
         voice.save("voice.mp3")
         playsound("voice.mp3")
         os.remove("voice.mp3")
